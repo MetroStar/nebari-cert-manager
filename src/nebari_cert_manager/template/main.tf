39c1e2a3dcc3967e20dae57dbfa4f46e5ccd6591
padili-metrostar <padili@metrostar.com>
2024-01-11 17:23:07 +0000

    Add Terraform templates

---
+++ src/nebari_cert_manager/template/main.tf
@@ -0,0 +1,43 @@
+locals {
+  name                = var.name
+  domain              = var.domain
+
+  create_namespace = var.create_namespace
+  namespace        = var.namespace
+  overrides        = var.overrides
+
+  solver_type = var.solver_type
+
+  chart_namespace = local.create_namespace ? kubernetes_namespace.this[0].metadata[0].name : local.namespace
+}
+
+resource "kubernetes_namespace" "this" {
+  count = local.create_namespace ? 1 : 0
+
+  metadata {
+    name = local.namespace
+  }
+}
+
+resource "helm_release" "this" {
+  name      = local.name
+  chart     = "./chart"
+  namespace = local.chart_namespace
+
+  dependency_update = true
+
+  values = [
+    yamlencode({
+      certificates = {
+        dnsNames = [local.domain, *.${local.domain}]
+      }
+      issuers = {
+        solver = {
+          type = local.solver_type
+          existingSecret = local.solver_type == "cloudflare" ? kubernetes_secret.auth[0].metadata[0].name : ""
+        }
+      }
+    }),
+    yamlencode(local.overrides),
+  ]
+}
---
+++ src/nebari_cert_manager/template/variables.tf
@@ -0,0 +1,26 @@
+variable "name" {
+  description = "Chart name"
+  type        = string
+}
+
+variable "domain" {
+  description = "Domain"
+  type        = string
+}
+
+variable "create_namespace" {
+  type = bool
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "overrides" {
+  type    = map(any)
+  default = {}
+}
+
+variable "solver_type" {
+  type = string
+}
