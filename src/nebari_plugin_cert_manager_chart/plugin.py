a1c6b895e919a310b92bb8383922f0c504142728
padili-metrostar <padili@metrostar.com>
2024-01-12 16:43:04 +0000

    Refactor line to get zone

--- src/nebari_plugin_cert_manager_chart/plugin.py
+++ src/nebari_plugin_cert_manager_chart/plugin.py
@@ -23,7 +23,7 @@ class CertManagerStage(NebariTerraformStage):
 
     def input_vars(self, stage_outputs: Dict[str, Dict[str, Any]]):
         domain = stage_outputs["stages/04-kubernetes-ingress"]["domain"]
-        zone = domain.split(".")[-2:]
+        zone = ".".join(domain.split(".")[-2:])
         chart_ns = self.config.cert_manager.namespace
         create_ns = True
         if chart_ns == None or chart_ns == "" or chart_ns == self.config.namespace:
